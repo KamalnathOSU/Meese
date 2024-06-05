b70710e9045cac5a05071b13454f3eb4464f8ad5
Kamalnath <Kamal@WSL_Ubuntu_20>
2024-06-05 16:50:07 -0500

    Updating LaplaceYoungK

---
+++ src/materials/LaplaceYoungK.C
@@ -0,0 +1,29 @@
+
+#include "LaplaceYoungK.h"
+
+registerMooseObject("MeeseApp", LaplaceYoungK);
+
+InputParameters
+LaplaceYoungK::validParams()
+{
+  InputParameters params = Material::validParams();
+
+  params.addRequiredCoupledVar("u","The variable to take the gradient of ");
+  params.addParam<MaterialPropertyName>("k","k","The name  of the property for k");
+  return params;
+}
+
+LaplaceYoungK::LaplaceYoungK(const InputParameters & parameters)
+  : Material(parameters),
+  _k(declareADProperty<Real>( getParam<MaterialPropertyName>("k") )),
+  _grad_u(adCoupledGradient("u"))
+   
+{
+  
+}
+
+void
+LaplaceYoungK::computeQpProperties()
+{
+   _k[ _qp ] = 1.0 / std::sqrt( 1 + _grad_u[_qp]*_grad_u[_qp] ); 
+}
